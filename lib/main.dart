04ce65e79259c93bdf6006d5d516d91b7c02a2ca
Khoa Nguyen <khoa.nguyen@adaptis.io>
2022-12-27 10:49:37 +0700

    Add styles for text widget

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -20,6 +21,7 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
+
   final String title;
 
   @override
@@ -27,24 +29,50 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Container(
-          margin: const EdgeInsets.only(top: 100.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              const Text("Hang thu nhat", style: TextStyle(fontSize: 24.0), textAlign: TextAlign.center),
-              Row(mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: const [
-                Text("Hang thu hai 1", style: TextStyle(fontSize: 24.0), textAlign: TextAlign.center),
-                Text("Hang thu hai 2", style: TextStyle(fontSize: 24.0), textAlign: TextAlign.center),
-              ]
-              ),
-              const Text("Hang thu ba bold 1", style: TextStyle(fontSize: 24.0), textAlign: TextAlign.center),
+          child: Container(
+        margin: const EdgeInsets.only(top: 100.0, left: 20.0, right: 20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            const Padding(
+                padding: EdgeInsets.symmetric(vertical: 5.0),
+                child: Text("Hang thu nhat",
+                    style: TextStyle(fontSize: 24.0, color: Colors.grey),
+                    textAlign: TextAlign.center)),
+            Padding(
+                padding: const EdgeInsets.symmetric(vertical: 5.0),
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: const [
+                      Text("Hang thu hai 1",
+                          style: TextStyle(fontSize: 24.0, color: Colors.grey),
+                          textAlign: TextAlign.center),
+                      Text("Hang thu hai 2",
+                          style: TextStyle(fontSize: 24.0, color: Colors.grey),
+                          textAlign: TextAlign.center),
+                    ])),
+            const Padding(
+                padding: EdgeInsets.symmetric(vertical: 5.0),
+                child: Text.rich(
+                  TextSpan(
+                      text: 'Hang thu ba ',
+                      style: TextStyle(fontSize: 20.0, color: Colors.grey),
+                      children: <TextSpan>[
+                        TextSpan(
+                          text: 'bold 1',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.yellow),
+                        )
+                      ]),
+                )),
+            const Divider(
+              thickness: 5.0,
+            )
           ],
         ),
       )),
